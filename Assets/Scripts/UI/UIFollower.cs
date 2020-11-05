3da497ea4ab1acf381b53e1eaa9332a445821a41
SomiWang王壽敏 <35202@oitc.com.tw>
2020-11-05 21:27:41 +0800

    temp

---
+++ Assets/Scripts/UI/UIFollower.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+[ExecuteInEditMode]
+public class UIFollower : MonoBehaviour
+{
+    [SerializeField]
+    GameObject m_Target;
+
+    // Update is called once per frameㄌ
+    void Update()
+    {
+        transform.position = m_Target.transform.position;
+        transform.rotation = m_Target.transform.rotation;
+    }
+}
